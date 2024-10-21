4ea16f7198a4e44a36c74599cb4302c4e8c2d2e3
Castona <castona4321@gmail.com>
2024-10-21 09:03:48 +0000

    group folder made

---
+++ group_01/pearson/original/vector.hpp
@@ -0,0 +1,31 @@
+/*
+Author: David Holmqvist <daae19@student.bth.se>
+*/
+#pragma once
+#if !defined(VECTOR_HPP)
+#define VECTOR_HPP
+
+class Vector {
+private:
+    unsigned size;
+    double* data;
+
+public:
+    Vector();
+    Vector(unsigned size);
+    Vector(unsigned size, double* data);
+    Vector(const Vector& other);
+    ~Vector();
+
+    double magnitude();
+    double mean();
+    double dot(Vector& rhs);
+
+    unsigned get_size() const;
+    double* get_data();
+
+    Vector operator/(double div);
+    Vector operator-(double sub);
+};
+
+#endif
