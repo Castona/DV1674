96745c0b208b7b6c01b48f07106d2070bdd7db9a
dapnix <niclas-andersson@outlook.com>
2024-10-21 11:02:31 +0200

    Blur and blur_parr now proper cpp arg stuff

--- blur/blur.cpp
+++ blur/blur.cpp
@@ -10,18 +10,10 @@ Author: David Holmqvist <daae19@student.bth.se>
 
 int main(int argc, char const* argv[])
 {
-    //if (argc != 4) 
-    if (argc < 4 || argc > 5) //Only allow arg counts of 4 and 5
+    if (argc != 4) 
     {
-        std::cerr << "Usage: " << argv[0] << " [radius] [infile] [outfile] [optional: threads]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [radius] [infile] [outfile]" << std::endl;
         std::exit(1);
-    }
-
-    int threadCount = 1; //Default to 1
-    if(argc == 5) //If we've specified another threadcount in the arguments, pick that one
-    {
-        std::string threadString = argv[4];
-        threadCount = std::stoi(threadString);
     }
 
     PPM::Reader reader {};
@@ -30,8 +22,7 @@ int main(int argc, char const* argv[])
     auto m { reader(argv[2]) };
     auto radius { static_cast<unsigned>(std::stoul(argv[1])) };
 
-    //auto blurred { Filter::blur(m, radius) };
-    Matrix blurred = Filter::blurParallel(m, radius, threadCount);
+    auto blurred { Filter::blur(m, radius) };
     writer(blurred, argv[3]);
 
     return 0;
